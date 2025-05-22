e9d2d5f41dfc5102d19684a36b7a82896713827a
Elbek <elbek-hacker@gmail.com>
2025-05-22 11:38:14 +0500

    1-dars. if for while vazifalar

--- 1-dars/sinf_ishi/1.py
+++ 1-dars/sinf_ishi/1.py
@@ -1,2 +1,5 @@
-print("2 dars")
-print("salom")
+matn = "bugun 1 dars lekin 3 ta dars otdik"
+
+for i in range(0, len(matn)):
+    #print(matn[i], end = '')
+    print(chr(ord(matn[i]) + 1), end='' )
---
+++ 1-dars/sinf_ishi/misol1.py
@@ -0,0 +1,7 @@
+a = 40
+i = 1
+while 1:
+    if i * i > a:
+        print(i)
+        break
+    
